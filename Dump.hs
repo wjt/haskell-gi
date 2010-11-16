ade710403ab6535bcf46b663caa8352659d7f938
Dafydd Harries <daf@rhydd.org>
2010-11-15 20:19:39 -0500

    make dump tool take name of typelib

--- Dump.hs
+++ Dump.hs
@@ -1,12 +1,15 @@
 module Main where
 
 import Control.Monad (forM_)
-import GI.API
+import System.Environment (getArgs)
+
 import Graphics.UI.Gtk
 import System.Glib.GError
 
+import GI.API
+
 main = handleGError (\(GError dom code msg) -> print (dom, code, msg)) $ do
     initGUI
-    apis <- loadAPI "TelepathyGLib"
+    [name] <- getArgs
+    apis <- loadAPI name
     forM_ apis $ \api -> print api
-
